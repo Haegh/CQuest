dff77b9014c2989e03094692b1243539f52a8f1e
Haegh <hughs_111@hotmail.fr>
2018-10-05 22:43:00 +0200

    create world and more + add ui component

---
+++ CQuest/CCore/NumberAssigner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCore {
+	public static class NumberAssigner {
+		static int _nextNumber = 0;
+
+		public static int GetNextNumber() {
+			_nextNumber = (_nextNumber + 1);
+
+			return _nextNumber;
+		}
+	}
+}
--- CQuest/CCore/Player.cs
+++ CQuest/CCore/Player.cs
@@ -5,11 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CCore {
-	public class Player {
-		public int CurrentHitPoints { get; set; }
-		public int MaximumHitPoints { get; set; }
+	public class Player : LivingCreature {
 		public int Gold { get; set; }
 		public int ExperiencePoints { get; set; }
 		public int Level { get; set; }
+		public List<InventoryItem> Inventory { get; set; }
+		public List<PlayerQuest> Quest { get; set; }
+
+		public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level ) : base (maximumHitPoints, currentHitPoints) {
+			Gold = gold;
+			ExperiencePoints = experiencePoints;
+			Level = level;
+			Inventory = new List<InventoryItem>();
+			Quest = new List<PlayerQuest>();
+		}
 	}
 }
